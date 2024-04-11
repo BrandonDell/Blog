cba0fb76eceabe5695d14973e91c5250e602d94a
Brandon Dell Cioppia <brandondell@brandons-mbp.lan>
2024-04-11 16:50:10 -0400

    final touches on localStorage and displaying data in blog.html

--- Assets/js/blog.js
+++ Assets/js/blog.js
@@ -1,7 +1,3 @@
-const blogInfo = localStorage.getItem("blogPosts");
-console.log(blogInfo);
-console.log(JSON.parse(blogInfo));
-
 const displayNewPost = function () {
     const postContent = JSON.parse(localStorage.getItem('allPosts'));
 
@@ -10,27 +6,18 @@ const displayNewPost = function () {
     let h1Element = document.createElement('h1');
     let pElement = document.createElement('p');
     let h2Element = document.createElement('h2');
-
-    h1Element.textContent = postContent[i].text;
+// gave content to the title, username, and content
+    h1Element.textContent = postContent[i].title;
     pElement.textContent = postContent[i].content;
     h2Element.textContent = postContent[i].username;
-
-    // h1.classList.add('newTitle');
-    // p.classList.add('postContent');
-    // h2.classList.add('newUsername');
-
-    // div.append(h1);
-    // div.append(p);
-    // div.append(h2);
-    let div = '<tr><td>Username</td><td>Title</td><td>Content</td></tr>';
-
-    document.querySelector('#blogTableBody').append(div);
+    // append them to the div element to create a box
+    divElement.appendChild(h1Element);
+    divElement.appendChild(pElement);
+    divElement.appendChild(h2Element);
+    console.log(divElement);
+    
+    document.querySelector('#blogTableBody').appendChild(divElement);
   }
 };
-
 displayNewPost()
 
-
-// document.main.append(div);
-// document.getSelection.append('article');
-// document.querySelector('main').append(div)
--- Assets/js/form.js
+++ Assets/js/form.js
@@ -4,28 +4,34 @@ document.getElementById('Form').addEventListener('submit', function (event) {
   let username = document.getElementById('username').value;
   let title = document.getElementById('title').value;
   let content = document.getElementById('content').value;
+// Commented out the alert as I liked the "required part on index.html"
+  // if (username && title && content) {
+    console.log('Username:', username);
+    console.log('Title:', title);
+    console.log('Content:', content);
+    blogInfo = {
+      username: username,
+      title: title,
+      content: content,
+    };
+    // long verson:let allPosts = localStorage.getItem('allPosts');
+    // console.log(allPosts);
+    // let posts = JSON.parse(allPosts);
+    // console.log(posts);
 
-  console.log('Username:', username);
-  console.log('Title:', title);
-  console.log('Content:', content);
-  blogInfo = {
-    username: username,
-    title: title,
-    content: content,
-  };
-  //   if ((blogInfo.userName == null || blogInfo.userName == "") || (blogInfo == null))
-  // } else {}
-  // localStorage.setItem('blogPosts', JSON.stringify(blogInfo));
-  // console.log(blogInfo);
+    // if (posts === null)
+    //   console.log('empty');
+    //   posts = [];
+    // }
+    // condensed verson:
+    const posts = JSON.parse(localStorage.getItem('allPosts')) || [];
 
-  const allPosts = localStorage.getItem('allPosts');
-  console.log(allPosts);
-  let posts = JSON.parse(allPosts);
-  console.log(posts);
-  // const allPosts = JSON.parse(getBlogInfo) || [];
-  posts.push(blogInfo);
-  console.log(posts);
-  localStorage.setItem('allPosts', JSON.stringify(posts));
+    posts.push(blogInfo);
+    console.log(posts);
+    localStorage.setItem('allPosts', JSON.stringify(posts));
 
-  window.location.href = 'blog.html';
+    window.location.href = 'blog.html';
+  // } else {
+  //   alert('Please fill out all fields');
+  // }
 });
